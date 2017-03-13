2de5b5e6028895c4a98c7c823eb682ef4ca02e1a
Tomas Munoz <tumunoz@uc.cl>
2017-03-13 18:54:50 +0000

    Fixing errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @news = News.find(params[:news_id])
+    @comment = @news.comments.create(comment_params)
+    redirect_to news_path(@news)
+  end
+ 
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
